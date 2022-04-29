b64d580f065eb94e1c33f2b1eaf59f44ce42c057
AveryMadness <averyfd@shaw.ca>
2022-04-29 08:53:55 -0600

    Create register.php

---
+++ source/register.php
@@ -0,0 +1,70 @@
+// Copyright (C) 2022 Avery Fiebig-Dorey
+
+<?php
+
+$DATABASE_HOST = 'localhost';
+$DATABASE_USER = 'root';
+$DATABASE_PASS = '';
+$DATABASE_NAME = 'averygame';
+$con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
+if (mysqli_connect_errno()) {
+	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
+}
+if (!isset($_POST['username'], $_POST['password'], $_POST['email'])) {
+	exit('Please complete the registration form!');
+}
+if (empty($_POST['username']) || empty($_POST['password']) || empty($_POST['email'])) {
+	exit('Please complete the registration form');
+}
+if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+	exit('Email is not valid!');
+}
+if (preg_match('/^[a-zA-Z0-9]+$/', $_POST['username']) == 0) {
+    exit('Username is not valid!');
+}
+if (strlen($_POST['password']) > 20 || strlen($_POST['password']) < 5) {
+	exit('Password must be between 5 and 20 characters long!');
+}
+if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+	$stmt->bind_param('s', $_POST['username']);
+	$stmt->execute();
+	$stmt->store_result();
+	if ($stmt->num_rows > 0) {
+		echo 'Username exists, please choose another!';
+	} else {
+if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)')) {
+	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+	$stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
+	$stmt->execute();
+	echo 'You have successfully registered, you can now login!';
+} else {
+	echo 'Could not prepare statement!';
+}
+	}
+	$stmt->close();
+} else {
+	echo 'Could not prepare statement!';
+}
+$con->close(); 
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Avery Game | Success</title>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+    <link href="style.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<div class="login">
+			<h1 id="MainText">Register Success!</h1>
+			<form action="authenticate.php" method="post">
+				<br>
+				<br>
+        <h2>You may now login to your account <a href="index.php">here</a></h2>
+        <br><br><br>
+			</form>
+		</div>
+	</body>
+</html>
